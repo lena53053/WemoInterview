e8a3abffd37d500559a8960b8634d91ba4552646
Lena Lai <lenal53053@gmail.com>
2020-04-07 17:09:09 +0800

    Initial Commit

---
+++ WemoInterview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WemoInterview
+//
+//  Created by Lena Lai on 4/7/20.
+//  Copyright © 2020 Lena Lai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
